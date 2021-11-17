a4aded8b9cc3b53e02b73ea205cd1f65a8e29b58
ShiridiKumar <p.shiridi@research.iiit.ac.in>
2021-11-17 10:06:15 +0530

    update package

--- algospackage.py
+++ algospackage.py
@@ -187,6 +187,22 @@ def knapSack(self,W, wt, val, n):
                 temp=dp[i-1][j-wt[i-1]]+val[i-1]
             dp[i][j]=max(temp,dp[i-1][j])
     return dp[n][W]
+
+def matrixMultiplication(self, N, arr):
+    dp=[[0 for i in range(N+1)] for j in range(N+1)]
+    for j in range(N-1,0,-1):
+        for k in range(j,N):
+            mi=float("inf")
+            if(j==k):
+                dp[j][k]=0
+                continue
+            for i in range(j,k):
+                temp=dp[j][i]+dp[i+1][k]+arr[j-1]*arr[i]*arr[k]
+                mi=min(temp,mi)
+            dp[j][k]=mi
+    return dp[1][N-1]
+
+
 def nCr(self, n, r):
     dp=[[0 for i in range(r+1)] for j in range(n+1)]
     for i in range(n+1):
@@ -207,7 +223,73 @@ def matrix_multiplication(self,s,n,arr,dp):
         dp[s][n]=mi
     return dp[s][n]
 
-"______________________________Driver code_______________________________________________"
+def editDistance(self, s, t):
+    dp=[[0 for i in range(len(t)+1)] for j in range(len(s)+1)]
+    for i in range(len(dp)):
+        dp[i][0]=i
+    dp[0]=[i for i in range(len(dp[0]))]
+    for i in range(1,len(dp)):
+        for j in range(1,len(dp[i])):
+            dp[i][j]=min(dp[i-1][j]+1,dp[i][j-1]+1,dp[i-1][j-1]+(0 if s[i-1]==t[j-1] else 1))
+    return dp[len(s)][len(t)]
+
+def equalPartition(self, N, arr):
+    dp=[[False for i in range(sum(arr)//2+1)] for j in range(N+1)]
+    if(sum(arr)%2==1):
+        return 0
+    dp[0]=[False for i in range(len(dp[0]))]
+    for i in range(len(dp)):
+        dp[i][0]=True
+    for i in range(1,len(dp)):
+        for j in range(1,len(dp[i])):
+            temp=dp[i-1][j-arr[i-1]] if(arr[i-1]<=j) else False
+            dp[i][j]=temp or dp[i-1][j]
+    return dp[N][sum(arr)//2]
+
+def lcs(self,x,y,str1,str2):
+    dp=[[0 for i in range(len(str2)+1)] for j in range(len(str1)+1)]
+    for i in range(1,len(str1)+1):
+        for j in range(1,len(str2)+1):
+            if(str1[i-1]==str2[j-1]):
+                dp[i][j]=1+dp[i-1][j-1]
+            else:
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+    return dp[len(str1)][len(str2)]
+
+def maximizeTheCuts(self,n,x,y,z):
+    dp=[-1 for i in range(n+1)]
+    dp[0]=0
+    for i in range(1,n+1):
+        p,q,r=-1,-1,-1
+        if(i-x>=0):
+            p=(1+dp[i-x]) if dp[i-x]!=-1 else -1
+        if(i-y>=0):
+            q=(1+dp[i-y]) if dp[i-y]!=-1 else -1
+        if(i-z>=0):
+            r=(1+dp[i-z]) if dp[i-z]!=-1 else -1
+        dp[i]=max(p,q,r)
+    return max(0,dp[n])
+
+def LongestRepeatingSubsequence(self, str):
+    dp=[[0 for i in range(len(str)+1)] for j in range(len(str)+1)]
+    for i in range(1,len(str)+1):
+        for j in range(1,len(str)+1):
+            if(str[i-1]==str[j-1] and i!=j):
+                dp[i][j]=1+dp[i-1][j-1]
+            else:
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+    return dp[len(str)][len(str)]
+
+
+def longestSubsequence(self,a,n):
+    dp=[1 for i in range(len(a))]
+    for i in range(len(a)):
+        ma=0
+        for j in range(i):
+            if(a[i]>a[j]):
+                ma=max(dp[j],ma)
+        dp[i]=1+ma
+    return max(dp)
 
 
 from collections import defaultdict
@@ -238,6 +320,86 @@ for i in range(n):
 
 a=diameter_tree(g[1])
 print(a)
+
+
+from collections import defaultdict
+class Graph1:
+    def __init__(self,vertices):
+        self.vertices=vertices
+        self.graph=defaultdict(list)
+
+    def addedge(self,u,v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def prims(self,l,source,edges):
+        exp=[source];queue=[];nodes=[]
+        count=0;cost=0
+        vis=[float("inf")]*(self.vertices+1)
+        visited=[False]*(self.vertices+1);vis[source]=0
+        while(count!=self.vertices-1):
+            for i in self.graph[source]:
+                if(l[source][i]<vis[i] and visited[i]!=True):
+                    vis[i]=l[source][i]
+            ind=0;m=float("inf")
+            visited[source]=True
+            for i in range(len(vis)):
+                if(not(visited[i]) and vis[i]<m):
+                    m=vis[i];ind=i
+            source=ind
+            count+=1
+        return vis
+    def dijikstra(self,d,l,source):
+        vis=[False]*(self.vertices+1)
+        count=1
+        while(count!=self.vertices):
+            for i in self.graph[source]:
+                if(d[source]+l[source][i]<d[i]):
+                    d[i]=d[source]+l[source][i]
+            ind=0;m=float("inf");vis[source]=True
+            count+=1
+            for i in range(len(vis)):
+                if(not(vis[i])):
+                    if(d[i]<m):
+                        m=d[i]
+                        ind=i
+            source=ind
+        for i in range(len(d)):
+                if(d[i]==float("inf")):
+                    d[i]=-1
+        return d
+        
+def prims(n, edges, start):
+    g=Graph1(n)
+    l=[ [i for i in range(n+1)] for j in range(n+1)]
+    for i in range(len(edges)):
+        u,v,cost=edges[i][0],edges[i][1],edges[i][2]
+        l[u][v]=cost;l[v][u]=cost
+        g.addedge(u,v)
+    ans=g.prims(l,start,edges)
+    return sum(ans[1:])
+
+
+def dijikstra(n, edges, s):
+    g=Graph1(n)
+    l=[]
+    for i in range(n+1):
+        s1=[]
+        for j in range(n+1):
+            s1.append(float("inf"))
+        l.append(s1)
+    for i in range(len(edges)):
+        u,v,cost=edges[i][0],edges[i][1],edges[i][2]
+        if(cost<l[u][v]):
+            l[u][v]=cost;l[v][u]=cost
+            g.addedge(u,v)
+    source=s
+    d=[float("inf")]*(n+1)
+    d[source]=0
+    ans=g.dijikstra(d,l,source)
+    p=ans[1:source]
+    p.extend(ans[source+1:])
+    return (p)
 
 
 
