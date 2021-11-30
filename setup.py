91f0320a9a818ff457d80f7d735ad01c08ab6435
ShiridiKumar <p.shiridi@research.iiit.ac.in>
2021-12-01 02:12:19 +0530

    final submission

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from distutils.core import setup
 
 setup(
-    name="algotoolsshiridi",
-    version="0.0.9",
+    name="skpalgotools",
+    version="0.0.7",
     description="A one stop solution for solving your required algorithms",
     author_email="shiridikumarpeddinti836@gmail.com",
-    py_modules=["algospackage"],
+    py_modules=["skpalgotools"],
     package_dir={"":"src"},
     install_requires=["matplotlib","pandas","numpy","seaborn"]
 
